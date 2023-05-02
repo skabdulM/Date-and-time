4b7e23dc47422b3ec000f07149bb76f057cc97f5
Shaikh Abdul Mannan <abdulmannan8334@gmail.com>
2023-05-02 22:45:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("hello world");
+// console.warn("this is warning");
+// console.error("this is an erreor");
+// var no1=44;
+// var no2=22;
+// console.log(no1 +no2);
+function hello(no1,no2){
+    no3=(no1+no2);
+    return no3;
+}
+no3=hello(1,2);
+console.log(no3);
